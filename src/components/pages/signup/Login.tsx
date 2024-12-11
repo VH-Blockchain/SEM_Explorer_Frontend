e5406b83490099e67b138fca0d212981ffd9d60f
Ashwin Pal <ashwinpal.vhits@gmail.com>
2024-12-11 15:32:36 +0530

    add validation in login and signup page

--- src/components/pages/signup/Login.tsx
+++ src/components/pages/signup/Login.tsx
@@ -2,16 +2,28 @@ import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { Box } from "@mui/material";
 import "react-toastify/dist/ReactToastify.css";
-
+import * as Yup from "yup";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
 
 const Login = () => {
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
 
 
-  async function LoginUser(e: any) {
-    e.preventDefault();
-    const body = { email: email, password: password };
+  async function LoginUser(values: { email: string; password: string }) {
+    const { email, password } = values;
+    const body = { email, password };
+
+    // e.preventDefault();
+    // const body = { email: email, password: password };
     // console.log("body",body);
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -77,62 +89,85 @@ const Login = () => {
       <Box className="container-wrape">
         <div className="auth-wrapper">
           <div className="auth-inner">
-            <form onSubmit={LoginUser}>
-              <ToastContainer />
-              <h3>Login</h3>
+            <Formik
+              initialValues={{ email: "", password: "" }}
+              validationSchema={validationSchema}
+              onSubmit={LoginUser}
+            >
+              {({
+                isSubmitting 
+              })=>(
+                <Form>
+                  <ToastContainer />
+                  <h3>Login</h3>
+                  <div className="mb-3">
+                    <label>Email address</label>
+                    <Field
+                      type="email"
+                      name="email"
+                      className="form-control"
+                      placeholder="Enter email"
+                      // value={email}
+                      // onChange={(e: any) => setemail(e.target.value)}
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="text-danger"
+                    />
+                  </div>
 
-              <div className="mb-3">
-                <label>Email address</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Enter email"
-                  value={email}
-                  onChange={(e: any) => setemail(e.target.value)}
-                />
-              </div>
+                  <div className="mb-3">
+                    <label>Password</label>
+                    <Field
+                      type="password"
+                      name="password"
+                      className="form-control"
+                      placeholder="Enter password"
+                      // value={password}
+                      // onChange={(e: any) => setpassword(e.target.value)}
+                    />
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      className="text-danger"
+                    />
+                  </div>
 
-              <div className="mb-3">
-                <label>Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Enter password"
-                  value={password}
-                  onChange={(e: any) => setpassword(e.target.value)}
-                />
-              </div>
+                  <div className="mb-3">
+                    <div className="custom-control custom-checkbox">
+                      <input
+                        type="checkbox"
+                        className="custom-control-input"
+                        id="customCheck1"
+                      />
+                      <label
+                        className="custom-control-label"
+                        htmlFor="customCheck1"
+                      >
+                        Remember me
+                      </label>
+                    </div>
+                  </div>
 
-              <div className="mb-3">
-                <div className="custom-control custom-checkbox">
-                  <input
-                    type="checkbox"
-                    className="custom-control-input"
-                    id="customCheck1"
-                  />
-                  <label
-                    className="custom-control-label"
-                    htmlFor="customCheck1"
-                  >
-                    Remember me
-                  </label>
-                </div>
-              </div>
+                  <div className="d-grid">
+                    <button type="submit" disabled={isSubmitting} className="btn btn-primary">
+                      Submit
+                    </button>
+                  </div>
+                  <p className="forgot-password text-right">
+                    <a href="#">Forgot password?</a>
+                  </p>
+                  <br />
+                  <p className="forgot-password text-right">
+                    <span>Don't have an Acoount? </span>
+                    <a href="/#/sign-up">Sign Up here</a>
+                  </p>
+                </Form>
+              )}
 
-              <div className="d-grid">
-                <button type="submit" className="btn btn-primary">
-                  Submit
-                </button>
-              </div>
-              <p className="forgot-password text-right">
-                <a href="#">Forgot password?</a>
-              </p>
-              <br />
-              <p className="forgot-password text-right">
-                <span>Don't have an Acoount? </span>
-                <a href="/sign-up">Sign Up here</a>
-              </p>
-            </form>
+            </Formik>
+          
           </div>
         </div>
       </Box>
--- src/components/pages/signup/SignUp.tsx
+++ src/components/pages/signup/SignUp.tsx
@@ -3,7 +3,23 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
+const validationSchema = Yup.object({
+  firstname: Yup.string()
+    .min(2, "First name should be at least 2 characters")
+    .required("First name is required"),
+  lastname: Yup.string()
+    .min(2, "Last name should be at least 2 characters")
+    .required("Last name is required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
 const Signup = () => {
   const [firstname, setfirstname] = useState();
   const [lastname, setlastname] = useState();
@@ -11,8 +27,10 @@ const Signup = () => {
   const [password, setpassword] = useState();
 
 
-  async function UserSignup(e: any) {
-    e.preventDefault();
+  async function UserSignup(values: any) {
+    const { firstname, lastname, email, password } = values;
+
+    // e.preventDefault();
     const body = { "FirstName": firstname, "LastName": lastname, "email": email, "password": password }
 
     var myHeaders = new Headers();
@@ -75,57 +93,83 @@ const Signup = () => {
     <Box className="container-wrape">
       <div className="auth-wrapper">
         <div className="auth-inner">
-          <form onSubmit={UserSignup}>
-            <ToastContainer />
-            <h3>Sign Up</h3>
+          <Formik
+            initialValues={{ firstname: '', lastname: '', email: '', password: '' }}
+            validationSchema={validationSchema}
+            onSubmit={UserSignup}
+          >
+            {({
+              isSubmitting
+            })=>(
+              <Form>
+                <ToastContainer />
+                <h3>Sign Up</h3>
 
-            <div className="mb-2">
-              <label>First name</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="First name"
-                value={firstname}
-                onChange={(e: any) => setfirstname(e.target.value)}
-              />
-            </div>
+                <div className="mb-2">
+                  <label>First name</label>
+                  <Field
+                    type="text"
+                    name='firstname'
+                    className="form-control"
+                    placeholder="First name"
+                    // value={firstname}
+                    // onChange={(e: any) => setfirstname(e.target.value)}
+                  />
+                  <ErrorMessage name="firstname" component="div" className="text-danger" />
+                </div>
 
-            <div className="mb-2">
-              <label>Last name</label>
-              <input type="text" className="form-control" placeholder="Last name" value={lastname} onChange={(e: any) => setlastname(e.target.value)} />
-            </div>
+                <div className="mb-2">
+                  <label>Last name</label>
+                  <Field
+                   type="text" 
+                    name='lastname'
+                   className="form-control" 
+                   placeholder="Last name"
+                    // value={lastname} 
+                    // onChange={(e: any) => setlastname(e.target.value)} 
+                    />
+                  <ErrorMessage name="lastname" component="div" className="text-danger" />
+                </div>
 
-            <div className="mb-3">
-              <label>Email address</label>
-              <input
-                type="email"
-                value={email}
-                onChange={(e: any) => setemail(e.target.value)}
-                className="form-control"
-                placeholder="Enter email"
-              />
-            </div>
+                <div className="mb-3">
+                  <label>Email address</label>
+                  <Field
+                    type="email"
+                    name='email'
+                    // value={email}
+                    // onChange={(e: any) => setemail(e.target.value)}
+                    className="form-control"
+                    placeholder="Enter email"
+                  />
+                  <ErrorMessage name="email" component="div" className="text-danger" />
+                </div>
 
-            <div className="mb-3">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control"
-                value={password}
-                onChange={(e: any) => setpassword(e.target.value)}
-                placeholder="Enter password"
-              />
-            </div>
+                <div className="mb-3">
+                  <label>Password</label>
+                  <Field
+                    type="password"
+                    name="password"
+                    className="form-control"
+                    // value={password}
+                    // onChange={(e: any) => setpassword(e.target.value)}
+                    placeholder="Enter password"
+                  />
+                  <ErrorMessage name="password" component="div" className="text-danger" />
+                </div>
 
-            <div className="d-grid">
-              <button type="submit" className="btn btn-primary">
-                Sign Up
-              </button>
-            </div>
-            <p className="forgot-password text-right">
-              Already have an account? <Link to={`/sign-in`}>sign in here</Link>
-            </p>
-          </form>
+                <div className="d-grid">
+                  <button type="submit" className="btn btn-primary" disabled={isSubmitting}>
+                    Sign Up
+                  </button>
+                </div>
+                <p className="forgot-password text-right">
+                  Already have an account? <Link to={`/sign-in`}>sign in here</Link>
+                </p>
+              </Form>
+            )}
+
+          </Formik>
+        
         </div>
       </div>
     </Box>
